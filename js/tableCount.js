98a3d7f916fc08aa9e92e1bfa939301afa29cb58
Jake <jlee37@student.fitchburgstate.edu>
2017-11-09 11:48:54 -0500

    Multiple Tables supported

--- js/tableCount.js
+++ js/tableCount.js
@@ -1,15 +1,73 @@
 $( document ).ready(function() {
-  //console.log(data);
-  $.each(data, function(key, item)
-{
-  var minPer = item.minStock;
-  //console.log(item.lengths);
-  $.each(item.lengths, function(key1, item1){
+  $.getJSON( "count.json", function( data ) {
+
+  console.log(data);
+  //addJson();
+  $.each(data, function(key, item){ //Each table in the json object
+
+  createTable(item); //Create table and fill with data
+
+
+});
+
+function addJson(){
+  $.extend(data,{
+    switches:{
+      id: "switches",
+      name: "Switches",
+      minStock: "5", //Percentage of stock
+      titles: { //Table titles
+        id0: "Switch type",
+        id1: "Quanity",
+        id2: "Date Checked"
+      },
+      rows: {
+        x440: {
+          id: "x440",
+          date: "11/9/17",
+          qty: "4",
+          reqStock: "0"
+        }
+    }
+  }
+});
+
+
+var out = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
+
+$('<a href="data:' + out + '" download="data.json">download JSON</a>').appendTo('.thing');
+}
+
+function createTable(data){
+  //Create tables and retrun id
+  var tbl = $("#tmpTable").html();
+  var $tbl = $('<div>').append($.parseHTML(tbl));
+
+  //Set table id
+  $tbl.find('#tmpid').attr('id', ('tbl'+data.id));
+  //Set heading
+  $tbl.find("h3").append(data.name);
+  //Set table column headings
+  $.each(data.titles, function(key, item){
+    var markup = "<th>"+item+"</th>";
+    $tbl.find("thead tr").append(markup);
+  });
+
+  //Append html to document
+  $("#invTables").append($tbl.html());
+
+  //Set row data
+  fillRows(data, ('#tbl'+data.id));
+}
+
+function fillRows(data, id){
+  var minPer = data.minStock;
+  //console.log(item.rows);
+  $.each(data.rows, function(key1, item1){ //Each row in the json object
       // console.log(item1.id);
       // console.log(item1.date);
       // console.log(item1.qty);
       // console.log(item1.reqStock);
-
         var stockPer = (item1.qty / item1.reqStock);
         if (stockPer >= 1){
           //Stock above recommended
@@ -30,9 +88,8 @@ $( document ).ready(function() {
 
       //Add to table
       var markup = "  <tr title='"+title+"' class='"+cls+"'><td>"+ item1.id +"</td><td>"+item1.qty+"</td><td>"+item1.date+"</td></tr>";
-      $("#countTable tbody").append(markup);
-
-
+      $(id+" tbody").append(markup);
   });
-});
+}
+  });
 });
